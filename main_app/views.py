6b1b3eecd7758d1d41ad4e720353ed8788c2e1fd
tucker-chatfield <tucker.chatfield@gmail.com>
2024-09-12 16:26:57 -0500

    Furter progress

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Cat
+from .forms import FeedingForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def cat_index(request):
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+      'cat': cat, 'feeding_form': feeding_form
+      })
+  
+def add_feeding(request, cat_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('cat_detail', cat_id=cat_id)
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+  
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
